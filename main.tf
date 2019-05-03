a89b1919e845748e3b6d67639582acf2e1b65c54
Josh Mahowald <joshua.mahowald@gmail.com>
2019-05-03 14:06:29 -0500

    show more things to give demo

--- main.tf
+++ main.tf
@@ -2,8 +2,19 @@ resource "null_resource" "example" {
 }
 
 resource "aws_ssm_parameter" "foo" {
-    value = "foobar"
+    value = "dummyvalue"
     overwrite = true
     name = "/test/foobar"
     type = "String"
+}
+
+
+resource "aws_ssm_parameter" "bar" {
+    value = "dummyvalue"
+    overwrite = true
+    name = "/test/foo"
+    type = "String"
+}
+output "fooval" {
+    value = "${aws_ssm_parameter.foo.value}"
 }
